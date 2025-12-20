46837da32bc68585788fa0b4c3c258b3cb606a9b
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-12-20 17:00:25 -0500

    Add handler declarations

--- firmware/Core/Inc/usb_device.h
+++ firmware/Core/Inc/usb_device.h
@@ -2,12 +2,20 @@
  * usb_device.h
  *
  *  Created on: Dec 20, 2025
- *      Author: Utilisateur
+ *      Author: wandouuu
  */
 
 #ifndef INC_USB_DEVICE_H_
 #define INC_USB_DEVICE_H_
 
+#include "matrix_scan.h"
 
+// Keyboard matrix handler
+void matrix_handler(uint8_t key_state, uint8_t row, uint8_t col);
 
+// Rotary encoder (rotation) handler
+void encoder_handler(void); // temporary arguments
+
+// SSD1306 OLED display handler
+void display_handler(void); // temporary arguments
 #endif /* INC_USB_DEVICE_H_ */
