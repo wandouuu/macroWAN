a26a108b4b2c319d183d474da7c9bb7c9a1defb3
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-12-20 16:38:57 -0500

    Add USB handler files

---
+++ firmware/Core/Inc/usb_device.h
@@ -0,0 +1,13 @@
+/*
+ * usb_device.h
+ *
+ *  Created on: Dec 20, 2025
+ *      Author: Utilisateur
+ */
+
+#ifndef INC_USB_DEVICE_H_
+#define INC_USB_DEVICE_H_
+
+
+
+#endif /* INC_USB_DEVICE_H_ */
---
+++ firmware/Core/Src/usb_device.c
@@ -0,0 +1,8 @@
+/*
+ * usb_device.c
+ *
+ *  Created on: Dec 20, 2025
+ *      Author: Utilisateur
+ */
+
+
