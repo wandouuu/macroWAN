0f5449445dd8750c246a70e92a4e7bd48ddc21bf
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-01-01 13:21:58 -0500

    Change void to uint8_t return type for send key

--- firmware/Core/Src/usb_device.c
+++ firmware/Core/Src/usb_device.c
@@ -2,7 +2,18 @@
  * usb_device.c
  *
  *  Created on: Dec 20, 2025
- *      Author: Utilisateur
+ *      Author: wandouuu
  */
+
+ #include "usb_device.h"
+ #include "usbd_customhid.h"
+ #include "keymap.h"
+
+ uint8_t send_key(uint8_t key_state, uint8_t row, uint8_t col){
+    
+   
+
+ }
+
 
 
